b31f8752fc6167ef14c2b85cb213bc738075e3ad
Asymmetriq <winner4166@gmail.com>
2019-12-15 14:31:30 +0300

    Add files via upload

---
+++ Task 2 – Splay.cpp
@@ -0,0 +1,467 @@
+﻿#include <iostream>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <cmath>
+
+
+struct Node
+{
+	int64_t key;
+	std::string value;
+	Node* left, * right, * parent;
+
+	Node(const int64_t& k) : key(k), left(nullptr), right(nullptr), parent(nullptr) {}
+	Node(const int64_t& k, const std::string& v, Node* p) : key(k), value(v),
+		left(nullptr), right(nullptr), parent(p) {}
+};
+
+class SplayTree
+{
+	Node* root;
+	size_t size;
+
+	// Является ли правым потомком
+	bool isRight(Node* node)
+	{
+		if (node && node->parent)
+			if (node->parent->right == node)
+				return true;
+			else
+				return false;
+	}
+
+	// Вспомогательная функция поиска
+	std::pair<int, Node*> Find(const int64_t& key)
+	{
+		Node* node = root;
+		while (node)
+		{
+			if (node->key == key)
+				return { 2, node }; // Нода найдена
+			else if (node->key > key)
+				if (node->left)
+					node = node->left;
+				else
+					return { 0, node }; // Потенциальный левый потомок для Add
+			else if (node->key < key)
+				if (node->right)
+					node = node->right;
+				else
+					return { 1, node }; // Потенциальный правый потомок для Add
+		}
+		return { 0, nullptr };
+	}
+
+	// Вспомогательная функция слияния
+	void Merge(Node* left, Node* right)
+	{
+		if (!left && !right)
+			root = nullptr;
+		else if (!left)
+		{
+			root = right;
+			right->parent = nullptr;
+		}
+		else if (!right)
+		{
+			root = left;
+			left->parent = nullptr;
+		}
+		else
+		{
+			left = Max(left);
+			left->parent = nullptr;
+			left->right = right;
+			right->parent = left;
+		}
+	}
+
+	// Функции вращения 
+	void rotateL(Node* node)
+	{
+		Node* p = node->parent;
+		Node* r = node->right;
+		if (p != nullptr)
+			if (p->left == node)
+				p->left = r;
+			else
+				p->right = r;
+		Node* temp = r->left;
+		r->left = node;
+		node->right = temp;
+		node->parent = r;
+		r->parent = p;
+		if (temp != nullptr)
+			temp->parent = node;
+	}
+
+	void rotateR(Node* node)
+	{
+		Node* p = node->parent;
+		Node* l = node->left;
+		if (p != nullptr)
+			if (p->left == node)
+				p->left = l;
+			else
+				p->right = l;
+		Node* temp = l->right;
+		l->right = node;
+		node->left = temp;
+		node->parent = l;
+		l->parent = p;
+		if (temp != nullptr)
+			temp->parent = node;
+	}
+
+	// 3 случая для Splay()
+	void Zig(Node* node)
+	{
+		!isRight(node) ?
+			rotateR(node->parent) : rotateL(node->parent);
+	}
+
+	void ZigZig(Node* node, bool side)
+	{
+		if (side)
+		{
+			rotateL(node->parent->parent);
+			rotateL(node->parent);
+		}
+		else
+		{
+			rotateR(node->parent->parent);
+			rotateR(node->parent);
+		}
+	}
+
+	void ZigZag(Node* node, bool side)
+	{
+		if (side)
+		{
+			rotateL(node->parent);
+			rotateR(node->parent);
+		}
+		else
+		{
+			rotateR(node->parent);
+			rotateL(node->parent);
+		}
+	}
+
+	// Основная функция Splay
+	void Splay(Node* node)
+	{
+		while (node->parent != nullptr)
+		{
+			if (node->parent == root)
+				Zig(node);
+			else if (!isRight(node) && !isRight(node->parent)) // Левый, левый
+				ZigZig(node, 0);
+			else if (isRight(node) && isRight(node->parent))   // Правый, правый
+				ZigZig(node, 1);
+			else if (!isRight(node) && isRight(node->parent))  // Левый, правый
+				ZigZag(node, 0);
+			else											   // Правый, левый
+				ZigZag(node, 1);
+		}
+		root = node;
+	}
+
+	// Вспомогательная функция печати ноды
+	std::string printNode(Node* node)
+	{
+		std::string result;
+		result += '[' + std::to_string(node->key) + ' ' + node->value;
+		if (node->parent)
+			result += ' ' + std::to_string(node->parent->key);
+		result += ']';
+		return result;
+	}
+
+	// Функции для поиска максимума / минимуму от ноды
+	Node* Max(Node* node)
+	{
+		Node* temp = node;
+		if (temp)
+		{
+			while (temp->right)
+				temp = temp->right;
+			Splay(temp);
+			return temp;
+		}
+		else
+			return nullptr;
+	}
+
+	Node* Min(Node* node)
+	{
+		Node* temp = node;
+		if (temp)
+		{
+			while (temp->left)
+				temp = temp->left;
+			Splay(temp);
+			return temp;
+		}
+		else
+			return nullptr;
+	}
+public:
+
+	Node* getRoot() { return root; }
+
+	SplayTree() : root(nullptr), size(0) { }
+
+	SplayTree(uint64_t key)
+	{
+		root = new Node(key);
+		size = 1;
+	}
+
+	~SplayTree()
+	{
+		if (root)
+		{
+			std::stack<Node*> toDelete;
+			toDelete.push(root);
+			while (!toDelete.empty())
+			{
+				Node* node = toDelete.top();
+				if (node->left)
+				{
+					toDelete.push(node->left);
+					node->left = nullptr;
+				}
+				else if (node->right)
+				{
+					toDelete.push(node->right);
+					node->right = nullptr;
+				}
+				else
+				{
+					toDelete.pop();
+					delete node;
+				}
+			}
+		}
+	}
+
+	bool Add(const int64_t& key, const std::string& value)
+	{
+		if (!root)
+		{
+			root = new Node(key, value, nullptr);
+			size++;
+			return true;
+		}
+		else
+		{
+			std::pair<int, Node*> result = Find(key);
+			if (result.first == 2)
+				return false;
+			else if (result.first == 0)
+			{
+				result.second->left = new Node(key, value, result.second);
+				Splay(result.second->left);
+				size++;
+				return true;
+			}
+			else
+			{
+				result.second->right = new Node(key, value, result.second);
+				Splay(result.second->right);
+				size++;
+				return true;
+			}
+		}
+	}
+
+	Node* Search(const int64_t& key)
+	{
+		if (root)
+		{
+			std::pair<int, Node*> result = Find(key);
+			if (result.first == 2)
+			{
+				Splay(result.second);
+				return result.second;
+			}
+			Splay(result.second);
+		}
+		return nullptr;
+	}
+
+	Node* Min() { return Min(root); }
+	Node* Max() { return Max(root); }
+
+	bool Set(const int64_t& key, const std::string& value)
+	{
+
+		if (!root)
+			return false;
+		std::pair<int, Node*> result = Find(key);
+		if (result.first == 2)
+		{
+			result.second->value = value;
+			Splay(result.second);
+			return true;
+		}
+		else
+			return false;
+	}
+
+	bool Delete(const int64_t& key)
+	{
+		if (!root)
+			return false;
+		std::pair<int, Node*> result = Find(key);
+		if (result.first == 2)
+		{
+			Splay(result.second);
+			Node* subL = result.second->left;
+			Node* subR = result.second->right;
+			Merge(subL, subR);
+			delete result.second;
+			size--;
+			return true;
+		}
+		return false;
+	}
+
+	std::string Print()
+	{
+		std::string result;
+		std::queue<Node*> toPrint;
+		toPrint.push(root);
+		size_t counter = size;
+		size_t space = 0;
+		size_t i = 0;
+		while (!toPrint.empty())
+		{
+			Node* node = toPrint.front();
+			toPrint.pop();
+			space++;
+
+			if (node)
+			{
+				result += printNode(node);
+				toPrint.push(node->left);
+				toPrint.push(node->right);
+				counter--;
+			}
+			else
+			{
+				result += "_";
+				toPrint.push(nullptr);
+				toPrint.push(nullptr);
+			}
+
+			if (space == pow(2, i))
+			{
+				result += "\n";
+				if (counter != 0)
+				{
+					i++;
+					space = 0;
+				}
+			}
+			else
+				result += " ";
+
+			if (counter == 0 && space == pow(2, i))
+				break;
+		}
+		return result;
+	}
+};
+
+
+int main()
+{
+	// Считываем все данные в строку
+	std::string data;
+	std::getline(std::cin, data, '\0');
+	data += '\n';
+
+	std::istringstream is(data);
+	std::ostringstream os;
+
+	SplayTree tree;
+
+	int64_t key;
+	std::string command, value;
+
+	bool empty = false; // флаг показывает, пуста ли поданная в поток ввода строка
+
+	while (is >> command)
+	{
+		if (command.empty())
+		{
+			empty = true;
+		}
+		if (command == "add" && is.peek() != '\n'
+			&& is >> key && is.peek() != '\n' && is >> value && is.peek() == '\n')
+		{
+			if (!tree.Add(key, value))
+				os << "error" << std::endl;
+		}
+		else if (command == "set" && is.peek() != '\n'
+			&& is >> key && is.peek() != '\n' && is >> value && is.peek() == '\n')
+		{
+			if (!tree.Set(key, value))
+				os << "error" << std::endl;
+		}
+		else if (command == "delete" && is.peek() != '\n'
+			&& is >> key && is.peek() == '\n')
+		{
+			if (!tree.Delete(key))
+				os << "error" << std::endl;
+		}
+		else if (command == "search" && is.peek() != '\n'
+			&& is >> key && is.peek() == '\n')
+		{
+			Node* temp = tree.Search(key);
+			if (temp)
+			{
+				os << "1 "
+					<< temp->value << std::endl;
+			}
+			else
+				os << "0" << std::endl;
+		}
+		else if (command == "min" && is.peek() == '\n')
+		{
+			Node* temp = tree.Min();
+			if (temp)
+			{
+				os << temp->key << " "
+					<< temp->value << std::endl;
+			}
+			else
+				os << "error" << std::endl;
+		}
+		else if (command == "max" && is.peek() == '\n')
+		{
+			Node* temp = tree.Max();
+			if (temp)
+			{
+				os << temp->key << " "
+					<< temp->value << std::endl;
+			}
+			else
+				os << "error" << std::endl;
+		}
+		else if (command == "print" && is.peek() == '\n')
+		{
+			os << tree.Print();
+		}
+		else
+		{
+			if (!empty) // если строка не пустая и не является ни одной из команд
+				os << "error" << std::endl;
+		}
+	}
+	std::cout << os.str();
+	return 0;
+}
---
+++ Task 3 – Heap.cpp
@@ -0,0 +1,314 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+
+struct Element
+{
+	int64_t key;
+	std::string value;
+};
+
+class Heap
+{
+	std::vector<Element> vec;
+	std::unordered_map<int64_t, int> map;
+
+	int getP(int i) { return (i - 1) / 2; }
+	int getL(int i) { return (2 * i + 1); }
+	int getR(int i) { return (2 * i + 2); }
+	
+	void siftUp(int index)
+	{
+		int64_t iKey = 0;
+		int64_t pKey = 0;
+
+		while (index > 0)
+		{
+			iKey = vec[index].key;
+			pKey = vec[getP(index)].key;
+
+			if (pKey > iKey)
+			{
+				std::swap(vec[index], vec[getP(index)]);
+				std::swap(map[pKey], map[iKey]);
+				index = getP(index);
+			}
+			else
+				break;
+		}
+	}
+
+	void siftDown(int index)
+	{
+		int left;
+		int right;
+		int smallest;
+
+		while (getL(index) < vec.size())
+		{
+			smallest = index;
+			left = getL(index);
+			right = getR(index);
+
+			if (left < vec.size() && vec[left].key < vec[smallest].key)
+				smallest = left;
+			if (right < vec.size() && vec[right].key < vec[smallest].key)
+				smallest = right;
+			if (smallest == index)
+				break;
+
+			std::swap(vec[index], vec[smallest]);
+			std::swap(map[vec[index].key], map[vec[smallest].key]);
+
+			index = smallest;
+		}
+	}
+
+	std::string printElement(int index)
+	{
+		std::string result = '[' + std::to_string(vec[index].key) + ' ' + vec[index].value;
+		if (index) 
+			result +=  ' ' + std::to_string(vec[getP(index)].key);
+		result+= ']';
+		return result;
+	}
+
+public:
+
+	Heap() {};
+	~Heap() {};
+
+	bool Add(int64_t& key, std::string& value)
+	{
+		if (map.count(key))
+			return false;
+
+		vec.push_back({ key, value });
+		map[key] = vec.size() - 1;
+		siftUp(vec.size() - 1);
+		return true;
+	}
+
+	bool Set(int64_t& key, std::string& value)
+	{
+		if (!map.count(key))
+			return false;
+
+		vec[map[key]].value = value;
+		return true;
+	}
+
+	bool Delete(int64_t& key)
+	{
+		if (map.count(key))
+		{
+			int index = map[key];
+			int64_t newkey = vec.back().key;
+
+			std::swap(vec[index], vec.back());
+			std::swap(map[vec[index].key], map[vec.back().key]);
+
+			vec.pop_back();
+			map.erase(key);
+			
+			if (newkey < key)
+				siftUp(index);
+			else
+				siftDown(index);
+			
+			return true;
+		}
+		return false;
+	}
+
+	std::tuple<bool, int, std::string> Search(int64_t& key)
+	{
+		if (!map.count(key))
+			return {0, 0, ""};
+		else
+			return { 1, map[key], vec[map[key]].value };
+	}
+
+	std::tuple<bool, int, Element> Min()
+	{
+		if (vec.size())
+			return { 1, 0, vec[0] };
+		else
+			return { 0, 0, {0,""} };
+	}
+
+	std::tuple<bool, int, Element> Max()
+	{
+		if (!vec.size())
+			return { 0, 0, {0,""} };
+		else
+		{
+			Element result = vec[0];
+			int index = 0;
+			for (size_t i = 0; i < vec.size(); i++)
+			{
+				if (vec[i].key > result.key)
+				{
+					result = vec[i];
+					index = i;
+				}
+			}
+			return { 1, index, result };
+		}
+	}
+
+	std::pair<bool, Element> Extract()
+	{
+		if (!vec.size())
+			return { 0, {0,""} };
+		else
+		{
+			Element result = vec[0];
+			map.erase(vec[0].key);
+			vec[0] = vec.back();
+			map[vec[0].key] = 0;
+			vec.pop_back();
+			siftDown(0);
+			return { 1, result };
+		}
+	}
+
+	std::string Print()
+	{
+		std::ostringstream result;
+		size_t space = 0;
+		size_t j = 0;
+
+		if (!vec.size())
+		{
+			result << "_\n";
+			return result.str();
+		}
+
+		for (size_t i = 0; i < vec.size(); i++)
+		{
+			result << printElement(i);
+			space++;
+
+			if (space == pow(2, j))
+			{
+				result << "\n";
+				if (i == vec.size() - 1)
+					return result.str();
+				j++;
+				space = 0;
+			}
+			else
+				result << " ";
+		}
+		for (; space != pow(2, j) - 1; space++)
+			result << "_ ";
+		result << "_\n";
+		return result.str();
+	}
+				
+};
+
+
+int main()
+{
+	// Считываем все данные в строку
+	std::string data;
+	std::getline(std::cin, data, '\0');
+	data += '\n';
+
+	std::istringstream is(data);
+	std::ostringstream os;
+
+	Heap heap;
+
+	int64_t key;
+	std::string command, value;
+
+	bool empty = false; // флаг показывает, пуста ли поданная в поток ввода строка
+
+	while (is >> command)
+	{
+		if (command.empty())
+		{
+			empty = true;
+		}
+		if (command == "add" && is.peek() != '\n'
+			&& is >> key && is.peek() != '\n' && is >> value && is.peek() == '\n')
+		{
+			if (!heap.Add(key, value))
+				os << "error" << std::endl;
+		}
+		else if (command == "set" && is.peek() != '\n'
+			&& is >> key && is.peek() != '\n' && is >> value && is.peek() == '\n')
+		{
+			if (!heap.Set(key, value))
+				os << "error" << std::endl;
+		}
+		else if (command == "delete" && is.peek() != '\n'
+			&& is >> key && is.peek() == '\n')
+		{
+			if (!heap.Delete(key))
+				os << "error" << std::endl;
+		}
+		else if (command == "search" && is.peek() != '\n'
+			&& is >> key && is.peek() == '\n')
+		{
+			std::tuple<bool, int, std::string> result = heap.Search(key);
+			if (std::get<0>(result))
+			{
+				os << "1 "
+					<< std::get<1>(result) << " "
+					<< std::get<2>(result) << std::endl;
+			}
+			else
+				os << "0" << std::endl;
+		}
+		else if (command == "min" && is.peek() == '\n')
+		{
+			std::tuple<bool, int, Element> result = heap.Min();
+			if (std::get<0>(result))
+			{
+				os << std::get<2>(result).key << " "
+					<< std::get<1>(result) << " "
+					<< std::get<2>(result).value << std::endl;
+			}
+			else
+				os << "error" << std::endl;
+		}
+		else if (command == "max" && is.peek() == '\n')
+		{
+			std::tuple<bool, int, Element> result = heap.Max();
+			if (std::get<0>(result))
+			{
+				os << std::get<2>(result).key << " "
+					<< std::get<1>(result) << " "
+					<< std::get<2>(result).value << std::endl;
+			}
+			else
+				os << "error" << std::endl;
+		}
+		else if (command == "extract" && is.peek() == '\n')
+		{
+			std::pair<bool, Element> result = heap.Extract();
+			if (result.first)
+				os << result.second.key << " " 
+				<< result.second.value << std::endl;
+			else
+				os << "error" << std::endl;
+		}
+		else if (command == "print" && is.peek() == '\n')
+		{
+			os << heap.Print();
+		}
+		else
+		{
+			if (!empty) // если строка не пустая и не является ни одной из команд
+				os << "error" << std::endl;
+		}
+	}
+	std::cout << os.str();
+	return 0;
+};
